676ff4574d2f1bc6b4e4e0d31945dcca40bcdd9c
Anshuman2510 <anshu25102015@gmail.com>
2022-08-21 19:04:48 +0530

    Resume-Builder

---
+++ src/config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDrqT0rijOgrMWktgbzEfDAKVuLV0_-S20",
+  authDomain: "resume-builder-b8597.firebaseapp.com",
+  projectId: "resume-builder-b8597",
+  storageBucket: "resume-builder-b8597.appspot.com",
+  messagingSenderId: "798706103470",
+  appId: "1:798706103470:web:9b0592315660706f4fa93f"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
